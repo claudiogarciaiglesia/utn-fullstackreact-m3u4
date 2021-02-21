5f6b71783d5d4a5726010076587057407f042c9c
Claudio Garcia Iglesia <claudio@fsdev.wonderbyte>
2021-02-21 14:56:29 -0300

    finish basic functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const util = require('util');
+
 // Configuracion de express
 const express = require('express');
 const app = express();
@@ -14,13 +16,81 @@ const conexion = mysql.createConnection({
 });
 conexion.connect();
 
-app.get('/', (req, res) => {
-    let list = 'list items here';
-    let newTaskButton = '<button> + </button>';
-    let html = '<h1>TODO LIST</h1><hr><ul>' + list + '</ul>' + newTaskButton;
+const qy = util.promisify(conexion.query).bind(conexion); // permite uso de async await con mysql
+
+app.get('/', async (req, res) => {
+    try {
+
+        let query = 'SELECT * FROM todolist';
+        let queryRes = await qy(query);
+
+        let list = '';
+
+        queryRes.forEach((task, index) => {
+            let dateFormatted = 'none';
+            if (task.date_limit) {
+                const date = new Date(task.date_limit);
+                dateFormatted = date.toISOString().substring(0, 10);
+            }
+            list = list + `<div class="list-row"><span class="task">${task.task}</span> <span class="due-date">Due date: ${dateFormatted}</span> <span class="checkbox"><input ${task.completed && 'checked'} type="checkbox"></span></div>`;
+        });
 
 
-    res.send(html);
+        let html =
+            `
+            <head>
+                <link rel="stylesheet" href="styles.css">
+            </head>
+            <body>
+                <h1>TODO LIST</h1>
+                <div class="list">${list}</div>
+                <form method="POST">
+                    <div class="new-task-container">
+                        <label for="task">Task
+                        <input type="text" name="task" required="required">
+                        </label>
+                        <label for="date_limit">Due date
+                        <input type="date" id="date_limit" name="date_limit">
+                        </label>
+                        <button type="submit">  +  </button>
+                    </div>        
+                </form>
+                
+            </body>
+        `;
+
+        res.send(html);
+    } catch (e) {
+        console.log(e.message);
+        res.status(413).send({ "Error": e.message });
+    }
+
+});
+
+// app.get('/newtask', (req, res) => {
+//     res.sendFile(__dirname + '/newtask.html');
+// });
+
+app.post('/', async (req, res) => {
+    console.log(req.body);
+    try {
+        if (!req.body.task) {
+            throw new Error('Task field is required');
+        };
+
+        let task = req.body.task;
+        let date_limit = req.body.date_limit;
+
+        let query = `INSERT INTO todolist (task, date_limit) VALUE ("${task}", ${date_limit ? '"' + date_limit + '"' : null})`;
+        queryRes = await qy(query);
+
+        console.log('tarea agregada');
+        res.redirect('/');
+
+    } catch (e) {
+        console.log(e.message);
+        res.status(413).send(`Error: ${e.message}`);
+    }
 });
 
 const PORT = process.env.PORT || 3000;
